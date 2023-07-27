d570a58a251a2d2ab82a3c835caa31d7f44b5452
davetech <davidrums65@gmail.com>
2023-07-27 18:28:06 +0100

    initialize amplify in frontend

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
